6cf9222f3f17156ba580ebfda8e90acb73c34687
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-06-11 08:44:18 -0400

    flatten array complete

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Part One
 let counter = 0;
 
 function incrementCounter() {
@@ -10,3 +11,27 @@ try {
 } catch (error) {
   console.log(error, counter);
 }
+
+//Part Two
+
+const myArr = [1, 2, 3, [4, 5], 6, 7, [8, [9]]];
+
+function flatArray(arr) {
+  let newArr = [];
+  flatten(0, arr, newArr);
+  return newArr;
+}
+
+function flatten(index, arr, newArr) {
+  if (index >= arr.length) return;
+
+  if (Array.isArray(arr[index])) {
+    flatten(0, arr[index], newArr);
+  } else {
+    newArr.push(arr[index]);
+  }
+
+  flatten(index + 1, arr, newArr);
+}
+
+console.log(flatArray(myArr));
