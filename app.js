b516805b4b19a0784d5c22dacdbe5736fad03720
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-06-11 10:45:47 -0400

    Part three done?

--- app.js
+++ app.js
@@ -14,20 +14,59 @@ try {
 
 //Part Two
 
-const myArr = [1, 2, 3, [4, 5], 6, 7, [8, [9]]];
+const myArr = [1, 2, 3, [4, 5], 6, 7, [8, [9, 10]]];
 
-let newArr = [];
-
-function flatten(index, arr) {
+function flatten(index, arr, newArr) {
   if (index >= arr.length) return newArr;
 
   if (Array.isArray(arr[index])) {
-    flatten(0, arr[index]);
+    flatten(0, arr[index], newArr);
   } else {
     newArr.push(arr[index]);
   }
 
-  return flatten(index + 1, arr);
+  return () => flatten(index + 1, arr, newArr);
 }
 
-console.log(flatten(0, myArr));
+const trampoline = (f, ...args) => {
+  let result = f(...args);
+  while (typeof result === "function") {
+    result = result();
+  }
+  return result;
+};
+
+const flattenedArray = trampoline(flatten, 0, myArr, []);
+console.log(flattenedArray);
+
+// Part 3
+
+const olEl = document.getElementById("result");
+console.log(olEl);
+
+function isPrime(num) {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function primeNumbers(start, n, newArr) {
+  if (start > n) return newArr;
+  if (isPrime(start)) {
+    const liEl = document.createElement("li");
+    liEl.textContent = start;
+    olEl.append(liEl);
+    newArr.push(start);
+  }
+  return () => primeNumbers(start + 1, n, newArr);
+}
+
+trampoline(primeNumbers, 2, 10000, []);
+
+setTimeout(() => {
+  alert("Calculation complete");
+}, 0);
